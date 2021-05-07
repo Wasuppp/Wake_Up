cb6437be84013895b21e1f912dc35a2f1bfc61de
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-05-07 15:30:11 +0100

    Antonio 7/05 | 15:30

---
+++ Assets/Scripts/Minijogos/TransporteMini.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransporteMini : MonoBehaviour
+{
+
+    private bool DentroPortalLab;
+
+    float Tempo;
+
+    [SerializeField] float TempoEsperaTransporte = 1f;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "TriggerMin")
+        {
+            DentroPortalLab = true;
+        }
+
+    }
+
+
+    private void Update()
+    {
+        if (DentroPortalLab == true)
+        {
+            Tempo += Time.deltaTime;
+
+            if (Tempo >= TempoEsperaTransporte) // se o jogador estiver dentro do portal mais de 2 segundos, é teletransportado para o nivel
+            {
+                SceneManager.LoadScene("Sala_Principal");
+            }
+        }
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/SalaPrincipal/Jogador.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jogador : MonoBehaviour
+{
+
+    private float Tempo = 0f;
+
+    [SerializeField] float TempoEsperaTransporte = 2f;
+
+    private bool DentroPortalLab = false; // O jogador está dentro do portal
+    private bool DentroPortalPass = false;
+    private bool DentroPortalMin = false;
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "TriggerLab")
+        {
+            DentroPortalLab = true;
+            Debug.Log("Portal_Labirinto");
+        }
+
+        if (other.tag == "TriggerPass")
+        {
+            DentroPortalLab = true;
+            Debug.Log("Portal_Passagens");
+        }
+
+        if (other.tag == "TriggerMin")
+        {
+            DentroPortalLab = true;
+            Debug.Log("Portal_MiniJogos");
+        }
+
+    }
+
+
+    private void Update()
+    {
+        if (DentroPortalLab == true)
+        {
+            Tempo += Time.deltaTime;
+
+            if(Tempo >= TempoEsperaTransporte) // se o jogador estiver dentro do portal mais de 2 segundos, é teletransportado para o nivel
+            {
+                TransporteLabirinto();
+            }
+        }
+
+        if (DentroPortalPass == true)
+        {
+            Tempo += Time.deltaTime;
+
+            if (Tempo >= TempoEsperaTransporte) // se o jogador estiver dentro do portal mais de 2 segundos, é teletransportado para o nivel
+            {
+                TransportePassagens();
+            }
+        }
+
+        if (DentroPortalMin == true)
+        {
+            Tempo += Time.deltaTime;
+
+            if (Tempo >= TempoEsperaTransporte) // se o jogador estiver dentro do portal mais de 2 segundos, é teletransportado para o nivel
+            {
+                TransportePassagens();
+            }
+        }
+
+    }
+
+
+    public void TransporteLabirinto()
+    {
+        SceneManager.LoadScene("Cubo_Labirinto");
+    }
+
+    public void TransportePassagens()
+    {
+        SceneManager.LoadScene("Passagens");
+    }
+
+    public void TranporteMinijogos()
+    {
+        SceneManager.LoadScene("Minijogos");
+    }
+}
+
