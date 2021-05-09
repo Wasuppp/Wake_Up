490e2e994b71bab471f3f6a6e8c9556f5a7ed9a4
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-05-09 18:26:26 +0100

    Antonio 09/05 | 18:26
    Fade In/out
    Legendas

---
+++ Assets/Scripts/CuboLabirinto/Legendas.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class Legendas : MonoBehaviour
+{
+    [SerializeField] GameObject Caixatexto;
+
+    void Start()
+    {
+
+        StartCoroutine(Sequencia());
+
+    }
+
+    IEnumerator Sequencia()
+    {
+        yield return new WaitForSeconds(1);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Michael - Como é que eu vim aqui parar?";
+        yield return new WaitForSeconds(3);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "";
+        yield return new WaitForSeconds(1);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Voz - Não te lembras? Tu estiveste sempre aqui!";
+        yield return new WaitForSeconds(3);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "";
+        yield return new WaitForSeconds(1);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Voz - O Mundo gira à tua volta.";
+        yield return new WaitForSeconds(3);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "";
+        yield return new WaitForSeconds(1);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Voz - Onde as duas faces se encontram, o caminho continua.";
+        yield return new WaitForSeconds(3);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "";
+        yield return new WaitForSeconds(6);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Voz - A resposta para o futuro está no presente.";
+        yield return new WaitForSeconds(20);
+
+        Caixatexto.GetComponent<TextMeshProUGUI>().text = "Voz - Procura a chave do teu passado na terceira rotação...";
+        yield return new WaitForSeconds(30);
+
+    }
+
+    // Michael - Como é que vim aqui parar?
+    // Narrador - Tu sempre estiveste aqui. Não te lembras?
+
+    // Narrador - Na tua mente, o Mundo gira à tua volta.
+    // Narrador - Onde as duas faces se encontram, o caminho continua.
+    // Narrador - Não percas a direção.
+    // Narrador - A resposta para o futuro está no presente...
+    // Narrador - Procura a chave do teu passado na terceira rotação...
+}
--- Assets/Scripts/SalaPrincipal/Jogador.cs
+++ Assets/Scripts/SalaPrincipal/Jogador.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Jogador : MonoBehaviour
 {
@@ -10,20 +11,29 @@ public class Jogador : MonoBehaviour
 
     [SerializeField] float TempoEsperaTransporte = 2f;
 
+    [SerializeField]  Image blackScreen;
+    [SerializeField] float tempoTransicaoPreto;
+
     bool DentroPortalLab = false; // O jogador está dentro do portal
     bool BloquearNivelLabirinto = false;
 
     bool DentroPortalPass = false;
     bool DentroPortalMin = false;
 
+    private void Start()
+    {
+        FadeFromBlack();
+    }
 
-    void OnTriggerStay(Collider other)
+
+        void OnTriggerStay(Collider other)
     {
         if (other.tag == "TriggerLab" && BloquearNivelLabirinto == false)
         {
             DentroPortalLab = true;
             Debug.Log("Portal_Labirinto");
             BloquearNivelLabirinto = true;
+            FadeToBlack();
         }
 
         if (other.tag == "TriggerPass")
@@ -89,6 +99,20 @@ public class Jogador : MonoBehaviour
     public void TranporteMinijogos()
     {
         SceneManager.LoadScene("Minijogos");
+    }
+
+    void FadeToBlack()
+    {
+        blackScreen.color = Color.black;
+        blackScreen.canvasRenderer.SetAlpha(0.0f);
+        blackScreen.CrossFadeAlpha(1.0f, tempoTransicaoPreto, false);
+    }
+
+    void FadeFromBlack()
+    {
+        blackScreen.color = Color.black;
+        blackScreen.canvasRenderer.SetAlpha(1.0f);
+        blackScreen.CrossFadeAlpha(0.0f, tempoTransicaoPreto, false);
     }
 }
 
