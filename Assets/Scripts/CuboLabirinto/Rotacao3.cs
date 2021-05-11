5b80e2eb6c0cf1d9b95925f862aa4383ab008198
Wasuppp <80390319+Wasuppp@users.noreply.github.com>
2021-05-11 11:00:59 +0100

    Add files via upload

---
+++ Assets/Scripts/CuboLabirinto/Rotacao3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao3 : MonoBehaviour
+{
+    [SerializeField] GameObject Cubo;
+    [SerializeField] GameObject ObjetoRotacao;
+
+
+    [SerializeField] float velocidadeRot = -20f;
+
+    bool Roda = false;
+    bool NaoRodaMais = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Roda = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Roda == true && NaoRodaMais == false)
+        {
+
+            Cubo.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 0, 1), velocidadeRot * Time.deltaTime);
+            Debug.Log("" + Cubo.transform.eulerAngles.z);
+
+            if (Cubo.transform.eulerAngles.z <= 90f)
+            {
+                Roda = false;
+                NaoRodaMais = true;
+            }
+        }
+    }
+}
