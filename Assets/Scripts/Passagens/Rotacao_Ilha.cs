35748a6287a0c9617b389bba1a9a69cae162f9b1
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-05-19 16:20:39 +0100

    Antonio 19/05 | 16:20

--- Assets/Scripts/Passagens/Rotacao_Ilha.cs
+++ Assets/Scripts/Passagens/Rotacao_Ilha.cs
@@ -4,60 +4,65 @@ using UnityEngine;
 
 public class Rotacao_Ilha : MonoBehaviour
 {
-    [SerializeField] LayerMask ImagemInteracao = 8;
+    // Este script é atribuído à Ilha, ou sala que vai rodar para permitir o jogador progrdir no nível
+    // A mecânica consiste em que o jogador aciona o objeto de escala situado no centro da ilha para rodar 90 graus no eixo y, para um sentido ou outro
+    // O problema é que, depois da 3ª rotação, se o jogador pressionar outra vez no objeto, a ilha começa a rodar infinitamente
 
-    [SerializeField] GameObject Ilha;
-    [SerializeField] GameObject ObjetoRotacao;
-    [SerializeField] float velocidadeRot = 20f;
-    [SerializeField] bool RotacaoPositiva = true;
 
-    bool EmRotacao = false;
+    [SerializeField] LayerMask ImagemInteracao = 8; // 
 
-    float graus = 0f;
+    [SerializeField] GameObject Ilha; //Sala que entra em rotação
+    [SerializeField] GameObject ObjetoRotacao; //Eixo de rotação da sala
+    [SerializeField] float velocidadeRot = 20f; //Velocidade de rotação da sala
+    [SerializeField] bool RotacaoPositiva = true; //Rotação a favor ou contra o sentido dos ponteiros do relogio
+    [SerializeField] int NumeroDeRotacoes = 3;
 
-    private Rigidbody Rb;
+    bool EmRotacao = false; //A sala está em rotação
 
-    private float contadorPositivo = 90f;
-    private float contadorNegativo = -90f;
+    private Rigidbody Rb; //Rigidbody da Sala
+
+    private float contadorPositivo = 90f; //Contador para a rotação poritiva
+    private float contadorNegativo = 270f;//Contador para a rotação poritivanegativa
 
     private void Start()
     {
-        Rb = GetComponent<Rigidbody>();
+        Rb = GetComponent<Rigidbody>(); //É salvo o Rigid Body da sala
     }
 
 
-    // Update is called once per frame
     void FixedUpdate()
     {
 
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, ImagemInteracao))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, ImagemInteracao)) // Se a direção da camara do jogador estiver sobre o objeto-escala
         {
-            if (Input.GetKeyDown(KeyCode.E) && EmRotacao == false)
+            if (Input.GetKeyDown(KeyCode.E) && EmRotacao == false) //se o jogador pressionar a tecla "E" e a sala estiver parada
             {
-                EmRotacao = true;
+                EmRotacao = true; // a sala roda
             }
         }
 
 
-        if (EmRotacao == true && RotacaoPositiva ==true) //rotacao no sentido dos ponteiros do relógio
+        if (EmRotacao == true && RotacaoPositiva ==true && NumeroDeRotacoes >0) //rotacao a favor do sentido dos ponteiros do relógio
         {
-            Ilha.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 1, 0), velocidadeRot * Time.deltaTime);
+            Ilha.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 1, 0), velocidadeRot * Time.deltaTime); //A sala roda à volta do objeto de rotação, pelo eixo y, a uma velocidade constante
 
-            if (Ilha.transform.eulerAngles.y >= contadorPositivo)
+            if (Ilha.transform.eulerAngles.y >= contadorPositivo) // se o valor da rotação em y for maior que o valor do contador, a rotação pára
             {
+                NumeroDeRotacoes -= 1;
                 EmRotacao = false;
-                contadorPositivo += 90f;
-            }
+                contadorPositivo += 90f; // o contador aumenta 90 graus
+            } 
         }
-        else if (EmRotacao == true && RotacaoPositiva == false) //rotacao contra o sentido dos ponteiros do relógio
+        else if (EmRotacao == true && RotacaoPositiva == false && NumeroDeRotacoes>0) //rotacao contra o sentido dos ponteiros do relógio
         {
-            Ilha.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 1, 0), velocidadeRot * Time.deltaTime);
+            Ilha.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 1, 0), -velocidadeRot * Time.deltaTime);
             Debug.Log(Ilha.transform.eulerAngles.y);
 
             if (Ilha.transform.eulerAngles.y <= contadorNegativo)
             {
+                NumeroDeRotacoes -= 1;
                 EmRotacao = false;
                 contadorNegativo -= 90f;
             }
@@ -69,7 +74,7 @@ public class Rotacao_Ilha : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.parent = transform;
+            other.transform.parent = transform; //Jogador passa para filho da sala
         }
     }
 }
