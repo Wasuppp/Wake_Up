1534bd2d4bbcef3ed4a10350ccd2b586586b5b96
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-06-02 13:21:34 +0100

    Antonio 02/06 | 13:21

---
+++ Assets/Scripts/Passagens/Botao.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao : MonoBehaviour
+{
+    Animation CarregaBotao;
+
+    [SerializeField] GameObject Escadas;
+    [SerializeField] GameObject ObjetoRotacao;
+    [SerializeField] float velocidadeRot;
+
+    float Contador;
+
+    bool BotaoPressionado = false;
+
+    private void Start()
+    {
+        CarregaBotao = gameObject.GetComponent<Animation>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CarregaBotao.Play("baixo");
+        BotaoPressionado = true;
+        Debug.Log("Botao");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        CarregaBotao.Play("baixo");
+    }
+
+    void RodarEscadas()
+    {
+        if (BotaoPressionado == true)
+        {
+
+            Escadas.transform.RotateAround(ObjetoRotacao.transform.position, new Vector3(0, 1, 0), velocidadeRot * Time.deltaTime); //A sala roda à volta do objeto de rotação, pelo eixo y, a uma velocidade constante
+
+            if (Escadas.transform.eulerAngles.y >= Contador) // se o valor da rotação em y for maior que o valor do contador, a rotação pára
+            {
+                BotaoPressionado = false;
+            }
+        }
+    }
+
+}
