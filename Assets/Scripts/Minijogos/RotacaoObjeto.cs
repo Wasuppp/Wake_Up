563e7ef4a7c0869f40259c8aece2b06048b359db
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-06-06 23:26:47 +0100

    Antonio 06/06 | 23:26

---
+++ Assets/Scripts/Minijogos/RotacaoObjeto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacaoObjeto : MonoBehaviour
+{
+    float VelocidadeRotacao = 20f;
+
+    private void Update()
+    {
+        transform.Rotate((Input.GetAxis("Mouse X") * VelocidadeRotacao * Time.deltaTime),
+         (Input.GetAxis("Mouse Y") * VelocidadeRotacao * Time.deltaTime), 0, Space.World);
+    }
+}
