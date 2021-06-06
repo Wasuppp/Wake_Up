592c60712c6e36d98a18ff5aa040acb7d9594b46
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-06-06 11:13:41 +0100

    Antonio 06/06 | 11:13

---
+++ Assets/Scripts/CuboLabirinto/GuiaNavMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuiaNavMesh : MonoBehaviour
+{
+    NavMeshAgent agente;
+    [SerializeField] Transform alvo;
+
+    bool PodeAndar = false;
+
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PodeAndar == true)
+        {
+            agente.destination = alvo.position;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            PodeAndar = true;
+        }
+    }
+}
