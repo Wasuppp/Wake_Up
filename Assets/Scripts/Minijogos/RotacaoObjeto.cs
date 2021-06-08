55566c9a7c9184c38ce42402d59aa792c2725730
Wasuppp <80390319+Wasuppp@users.noreply.github.com>
2021-06-08 19:18:40 +0100

    Diogo - 19:15 - 08/06
    Update

--- Assets/Scripts/Minijogos/RotacaoObjeto.cs
+++ Assets/Scripts/Minijogos/RotacaoObjeto.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RotacaoObjeto : MonoBehaviour
 {
     float VelocidadeRotacao = 20f;
 
+    CinemachineVirtualCamera currentCamera;
+
+    [SerializeField]
+    CinemachineVirtualCamera target;
+
     private void Update()
     {
         transform.Rotate((Input.GetAxis("Mouse X") * VelocidadeRotacao * Time.deltaTime),
          (Input.GetAxis("Mouse Y") * VelocidadeRotacao * Time.deltaTime), 0, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentCamera.Priority--;
+
+            currentCamera = target;
+
+            currentCamera.Priority++;
+        }
+
     }
 }
