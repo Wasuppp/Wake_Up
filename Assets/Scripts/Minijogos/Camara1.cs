118a1237970ef1635287787bf9236ec91d31a283
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-06-08 22:23:36 +0100

    Antonio 08/06 | 22:23

--- Assets/Scripts/Minijogos/Camara1.cs
+++ Assets/Scripts/Minijogos/Camara1.cs
@@ -7,26 +7,46 @@ public class Camara1 : MonoBehaviour
     [SerializeField] Camera jogador;
 
     [SerializeField] Camera camara1;
+    [SerializeField] Camera camara2;
 
     [SerializeField] GameObject objeto1;
+    [SerializeField] GameObject objeto2;
 
-    bool MudarCamara = false;
+    bool MudarCamara1 = false;
+    bool MudarCamara2 = false;
+    bool MudarCamara3 = false;
 
     void  Start()
     {
         objeto1.GetComponent<RotacaoObjeto>().enabled = false;
         jogador.enabled = true;
         camara1.enabled = false;
+        camara2.enabled = false;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.E) && MudarCamara == false)
+        if (other.CompareTag("Rotacao1"))
         {
-            objeto1.GetComponent<RotacaoObjeto>().enabled = true;
-            MudarCamara = true;
-            jogador.enabled = !jogador.enabled;
-            camara1.enabled = !camara1.enabled;
+            if (Input.GetKeyDown(KeyCode.E) && MudarCamara1 == false)
+            {
+                objeto1.GetComponent<RotacaoObjeto>().enabled = true;
+                MudarCamara1 = true;
+                jogador.enabled = !jogador.enabled;
+                camara1.enabled = !camara1.enabled;
+            }
         }
+        if (other.CompareTag("Rotacao2"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && MudarCamara2 == false)
+            {
+                objeto2.GetComponent<RotacaoObjeto>().enabled = true;
+                MudarCamara2 = true;
+                jogador.enabled = !jogador.enabled;
+                camara2.enabled = !camara2.enabled;
+            }
+        }
+
+
     }
 }
--- Assets/Scripts/Minijogos/RotacaoObjeto1.cs
+++ Assets/Scripts/Minijogos/RotacaoObjeto1.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class RotacaoObjeto : MonoBehaviour
 {
+    bool Xcorreto = false;
+    bool Ycorreto = false;
+    bool Zcorreto = false;
+
+    [SerializeField] GameObject ObjetoImpossivel;
+
     float VelocidadeRotacao = 20f;
 
     private void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
         transform.Rotate((Input.GetAxis("Mouse X") * VelocidadeRotacao * Time.deltaTime),
          (Input.GetAxis("Mouse Y") * VelocidadeRotacao * Time.deltaTime), 0, Space.World);
+        }
     }
 }
