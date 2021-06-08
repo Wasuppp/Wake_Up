1bd0f8336eed8febef3f1a50a9a8b2a06cb09bdb
Wasuppp <80390319+Wasuppp@users.noreply.github.com>
2021-06-08 19:55:35 +0100

    Diogo - 19:55 - 08/06
    Cinematic

---
+++ Assets/Scripts/Minijogos/Camara1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara1 : MonoBehaviour
+{
+
+    [SerializeField]
+    Camera jogador;
+
+    [SerializeField]
+    Camera camara1;
+
+    [SerializeField] 
+    LayerMask ImagemInteracao = 8;
+
+    [SerializeField]
+    GameObject objeto1;
+
+    bool MudarCamara = false;
+
+    void  Start()
+    {
+        objeto1.GetComponent<RotacaoObjeto>().enabled = false;
+        jogador.enabled = true;
+        camara1.enabled = false;
+    }
+    
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 30, ImagemInteracao))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && MudarCamara == false)
+            {
+                objeto1.GetComponent<RotacaoObjeto>().enabled = true;
+                MudarCamara = true;
+                jogador.enabled = !jogador.enabled;
+                camara1.enabled = !camara1.enabled;
+            }
+        }
+
+    }
+}
